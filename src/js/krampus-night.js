80798a83814c21a003e61da620892cc24c7f20b5
TheVirtuoid <thevirtuoid@gmail.com>
2021-12-15 21:34:19 -0500

    Final updates

--- src/js/krampus-night.js
+++ src/js/krampus-night.js
@@ -4,38 +4,3 @@ import PhaserGame from "./classes/game-engines/phaser/PhaserGame.js";
 const game = new Game({ engine: PhaserGame });
 game.start();
 
-/*
-import Phaser from 'phaser';
-import youTubeImage from './../img/youTubeImage.png';
-
-class KrampusNightGame extends Phaser.Scene {
-	constructor() {
-		super();
-	}
-
-	preload () {
-		this.load.image('logo', youTubeImage);
-	}
-
-	create () {
-		const logo = this.add.image(400, 140, 'logo');
-		this.tweens.add({
-			targets: logo,
-			y: 450,
-			duration: 2000,
-			ease: "Power2",
-			yoyo: true,
-			loop: -1
-		})
-	}
-}
-const config = {
-	type: Phaser.AUTO,
-	parent: 'phaser-example',
-	width: 800,
-	height: 600,
-	scene: KrampusNightGame
-}
-const game = new Phaser.Game(config);
-console.log('it works!');
-*/
